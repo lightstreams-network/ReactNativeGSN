a5727da97e89a5bf21682cc48bd2c79a7193bcad
Redmen Ishab <ishabachary@gmail.com>
2020-05-20 17:22:13 +0545

    private key and user address sent from params to sendTransaction

--- App.js
+++ App.js
@@ -1,115 +1,123 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import React, { Component } from "react";
+import {
+	StyleSheet,
+	Text,
+	View,
+	TouchableOpacity,
+	TextInput,
+	AsyncStorage
+} from "react-native";
 
-import Voter from './build/contracts/Voter.json';
+import Voter from "./build/contracts/Voter.json";
 
-const ethers = require('ethers');
-const crypto = require('./src/crypto');
+const ethers = require("ethers");
+const crypto = require("./src/crypto");
 
-const privateKey = "7dc79980cde90e81c3717e2ec03ff36f9afac2ce5ba4939ac54611c15bd22658"
+const privateKey =
+	"7dc79980cde90e81c3717e2ec03ff36f9afac2ce5ba4939ac54611c15bd22658";
 const url = "https://node.sirius.lightstreams.io:443";
 
 const provider = crypto.GsnProvider(url);
 const wallet = crypto.newWallet(privateKey, provider);
 
-
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      resultText: "",
-      contractDeployed: false
-    }
-  }
+	constructor() {
+		super();
+		this.state = {
+			resultText: "",
+			contractDeployed: false
+		};
+	}
 
-  
-  votePressed = async () => {
-    console.log("votePressed")
-    const voterAddress = "0x4C3Bf861A9F822F06c10fE12CD912AaCC5e3A4f6";
-    const abi = Voter.abi;
-    const contract = new ethers.Contract(voterAddress, abi, provider);
+	votePressed = async () => {
+		console.log("votePressed");
+		const voterAddress = "0x4C3Bf861A9F822F06c10fE12CD912AaCC5e3A4f6";
+		const abi = Voter.abi;
+		const contract = new ethers.Contract(voterAddress, abi, provider);
 
-    let contractWithSigner = contract.connect(wallet);
+		let contractWithSigner = contract.connect(wallet);
+		let payload = {
+			privateKey: privateKey,
+			address: voterAddress
+		};
 
+		let tx = await contractWithSigner.upVote();
+		//await tx.wait();
 
-    let tx = await contractWithSigner.upVote()
-    //await tx.wait();
+		let count = await contract.count();
 
-    
-    let count = await contract.count();
+		this.setState({
+			resultText: count.toString()
+		});
+	};
 
-    
-    this.setState({
-      resultText: count.toString(),
-    });
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<TouchableOpacity
+					style={styles.bigButton}
+					onPress={() => this.votePressed()}
+				>
+					<Text style={styles.buttonText}>Vote</Text>
+				</TouchableOpacity>
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.bigButton} onPress={() => this.votePressed()}>
-          <Text style={styles.buttonText}>Vote</Text>
-        </TouchableOpacity>
-        
-        <Text style={[{marginTop: 30}]}>
-        {this.state.resultText}</Text>
-      </View>
-    );
-  }
+				<Text style={[{ marginTop: 30 }]}>{this.state.resultText}</Text>
+			</View>
+		);
+	}
 }
 
 export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  setValueView: {
-    flex: 1,
-    flexDirection: 'row',
-    height: 100
-  },
-  bigButton: {
-    marginRight:40,
-    marginLeft:40,
-    marginTop:10,
-    width: 200,
-    paddingTop:20,
-    paddingBottom:20,
-    backgroundColor:'#68a0cf',
-    borderRadius:10,
-    borderWidth: 1,
-    borderColor: '#fff'
-  },
-  button: {
-    marginRight:40,
-    marginLeft:40,
-    marginTop:10,
-    width: 100,
-    paddingTop:10,
-    paddingBottom:10,
-    backgroundColor:'#76788f',
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#fff'
-  },
-  buttonText:{
-      color:'#fff',
-      textAlign:'center',
-  },
-  textInput: {
-    height: 40,
-    paddingLeft: 6,
-    borderBottomWidth: 1,
-    borderBottomColor: '#76788f'
-
-  },
-  hidden: {
-    opacity: 0,
-    width: 0,
-    height: 0,
-  }
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	setValueView: {
+		flex: 1,
+		flexDirection: "row",
+		height: 100
+	},
+	bigButton: {
+		marginRight: 40,
+		marginLeft: 40,
+		marginTop: 10,
+		width: 200,
+		paddingTop: 20,
+		paddingBottom: 20,
+		backgroundColor: "#68a0cf",
+		borderRadius: 10,
+		borderWidth: 1,
+		borderColor: "#fff"
+	},
+	button: {
+		marginRight: 40,
+		marginLeft: 40,
+		marginTop: 10,
+		width: 100,
+		paddingTop: 10,
+		paddingBottom: 10,
+		backgroundColor: "#76788f",
+		borderRadius: 5,
+		borderWidth: 1,
+		borderColor: "#fff"
+	},
+	buttonText: {
+		color: "#fff",
+		textAlign: "center"
+	},
+	textInput: {
+		height: 40,
+		paddingLeft: 6,
+		borderBottomWidth: 1,
+		borderBottomColor: "#76788f"
+	},
+	hidden: {
+		opacity: 0,
+		width: 0,
+		height: 0
+	}
 });
--- src/crypto/crypto.js
+++ src/crypto/crypto.js
@@ -1,18 +1,14 @@
 import {
 	RELAY_URL,
-    RELAY_HUB,
+	RELAY_HUB,
 	RELAY_ADRRESS,
 	BLOCKCHAIN_RPC,
 	CHAIN_ID,
 	CHAIN_NAME
 } from "react-native-dotenv";
-
 const inherits = require("inherits");
 const ethers = require("ethers");
-const { utils, Wallet, getDefaultProvider } = ethers;
 const Web3 = require("web3"); // This dependency needs to be removed
-const EthCrypto = require("eth-crypto"); // This dependency needs to be removed
-
 const web3 = new Web3(BLOCKCHAIN_RPC); // This dependency needs to be removed
 
 const network = {
@@ -28,17 +24,17 @@ function GsnProvider(url, network) {
 inherits(GsnProvider, ethers.providers.BaseProvider);
 
 function bytesToHex_noPrefix(bytes) {
-    let hex = removeHexPrefix(bytes)
-    if (hex.length % 2 != 0) {
-        hex = "0" + hex;
-    }
-    return hex
+	let hex = removeHexPrefix(bytes);
+	if (hex.length % 2 != 0) {
+		hex = "0" + hex;
+	}
+	return hex;
 }
 
 function toUint256_noPrefix(int) {
-    let hex = ethers.utils.hexlify(int)
-    let padded = ethers.utils.hexZeroPad(hex, 32)
-    return removeHexPrefix(padded);
+	let hex = ethers.utils.hexlify(int);
+	let padded = ethers.utils.hexZeroPad(hex, 32);
+	return removeHexPrefix(padded);
 }
 
 function removeHexPrefix(hex) {
@@ -46,83 +42,105 @@ function removeHexPrefix(hex) {
 }
 
 function parseHexString(str) {
-        var result = [];
-        while (str.length >= 2) {
-            result.push(parseInt(str.substring(0, 2), 16));
+	var result = [];
+	while (str.length >= 2) {
+		result.push(parseInt(str.substring(0, 2), 16));
 
-            str = str.substring(2, str.length);
-        }
+		str = str.substring(2, str.length);
+	}
 
-        return result;
-    }
+	return result;
+}
 
-function getTransactionHash (from, to, tx, txfee, gas_price, gas_limit, nonce, relay_hub_address, relay_address) {
-        let relay_prefix = "rlx:"
-        let txhstr = bytesToHex_noPrefix(tx)
-        let dataToHash =
-            Buffer.from(relay_prefix).toString("hex") +
-            removeHexPrefix(from)
-            + removeHexPrefix(to)
-            + txhstr
-            + toUint256_noPrefix(parseInt(txfee))
-            + toUint256_noPrefix(parseInt(gas_price))
-            + toUint256_noPrefix(parseInt(gas_limit))
-            + toUint256_noPrefix(parseInt(nonce))
-            + removeHexPrefix(relay_hub_address)
-            + removeHexPrefix(relay_address)
+function getTransactionHash(
+	from,
+	to,
+	tx,
+	txfee,
+	gas_price,
+	gas_limit,
+	nonce,
+	relay_hub_address,
+	relay_address
+) {
+	let relay_prefix = "rlx:";
+	let txhstr = bytesToHex_noPrefix(tx);
+	let dataToHash =
+		Buffer.from(relay_prefix).toString("hex") +
+		removeHexPrefix(from) +
+		removeHexPrefix(to) +
+		txhstr +
+		toUint256_noPrefix(parseInt(txfee)) +
+		toUint256_noPrefix(parseInt(gas_price)) +
+		toUint256_noPrefix(parseInt(gas_limit)) +
+		toUint256_noPrefix(parseInt(nonce)) +
+		removeHexPrefix(relay_hub_address) +
+		removeHexPrefix(relay_address);
 
-        let hash1 = ethers.utils.keccak256('0x' + dataToHash) 
-        let msg = Buffer.concat([Buffer.from("\x19Ethereum Signed Message:\n32"), Buffer.from(removeHexPrefix(hash1), "hex")])
-        let hash2 = ethers.utils.keccak256("0x"+msg.toString('hex') )
-        return hash2;
-    }
+	let hash1 = ethers.utils.keccak256("0x" + dataToHash);
+	let msg = Buffer.concat([
+		Buffer.from("\x19Ethereum Signed Message:\n32"),
+		Buffer.from(removeHexPrefix(hash1), "hex")
+	]);
+	let hash2 = ethers.utils.keccak256("0x" + msg.toString("hex"));
+	return hash2;
+}
 
 GsnProvider.prototype.perform = async function (method, params) {
-	console.log({ method });
+	console.log({ method, params });
 	if (method === "sendTransaction") {
-		// The voter address needs to come from the params
-		const voterAddress = "0x4C3Bf861A9F822F06c10fE12CD912AaCC5e3A4f6";
-		// The user's private key and account address needs to come from the params
-		const identity = await EthCrypto.createIdentity();
-        let key = new ethers.utils.SigningKey(identity.privateKey)
+		// from : user Address
+		let from = params.from;
+		// to: contract address
+		let to = params.to;
+		let tx = params.data;
+		let txfee = 70;
+		let gas_price = "500000000000";
+		let gas_limit = "28667";
+		let nonce = "0";
+		let relay_hub_address = RELAY_HUB;
+		let relay_address = RELAY_ADRRESS;
+		let privateKey = params.privateKey;
 
-        let from = identity.address;
-        let to = voterAddress;
-        let tx = params.data;
-        let txfee = 70;
-        let gas_price = "500000000000";
-        let gas_limit = "28667";
-        let nonce = "0";
-        let relay_hub_address = RELAY_HUB;
-        let relay_address = RELAY_ADRRESS;
+		let hash = getTransactionHash(
+			from,
+			to,
+			tx,
+			txfee,
+			gas_price,
+			gas_limit,
+			nonce,
+			relay_hub_address,
+			relay_address
+		);
+		let key = new ethers.utils.SigningKey(privateKey);
 
-        let hash = getTransactionHash(from, to, tx, txfee, gas_price, gas_limit, nonce, relay_hub_address, relay_address);
-        let signed = ethers.utils.joinSignature(key.signDigest(hash));
+		let signed = ethers.utils.joinSignature(key.signDigest(hash));
 
-        let relayMaxNonce = (await web3.eth.getTransactionCount(relay_address)) + 3;
+		let relayMaxNonce = (await web3.eth.getTransactionCount(relay_address)) + 3;
 
-        let jsonRequestData = {
-                "encodedFunction": tx,
-                "signature": parseHexString(signed.replace(/^0x/, '')),
-                "approvalData": [],
-                "from": from,
-                "to": to,
-                "gasPrice": parseInt(gas_price),
-                "gasLimit": parseInt(gas_limit),
-                "relayFee": txfee,
-                "RecipientNonce": parseInt(nonce),
-                "RelayMaxNonce": relayMaxNonce,
-                "RelayHubAddress": relay_hub_address
-            };
+		let jsonRequestData = {
+			encodedFunction: tx,
+			signature: parseHexString(signed.replace(/^0x/, "")),
+			approvalData: [],
+			from: from,
+			to: to,
+			gasPrice: parseInt(gas_price),
+			gasLimit: parseInt(gas_limit),
+			relayFee: txfee,
+			RecipientNonce: parseInt(nonce),
+			RelayMaxNonce: relayMaxNonce,
+			RelayHubAddress: relay_hub_address
+		};
 
-        let relayRes;
+		let relayRes;
 
-		try { 
+		try {
 			relayRes = await fetch(RELAY_URL + "/relay", {
 				method: "POST",
-                headers: {
-                'Content-Type': 'application/json'
-                },
+				headers: {
+					"Content-Type": "application/json"
+				},
 				body: JSON.stringify(jsonRequestData)
 			});
 			console.log("response from post request to relay", relayRes);
